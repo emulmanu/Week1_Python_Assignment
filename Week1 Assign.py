d7882dcdcafefa2466a17120b14e898ca965bab2
emulmanu <emuljr1@gmail.com>
2025-07-29 20:51:48 +0300

    Week1 Assign.py

---
+++ Week1 Assign.py
@@ -0,0 +1,21 @@
+# simple calculator
+num1 =float(input("Enter the first Number: "))
+num2 = float(input("Enter the second Number: "))
+operation = input("Enter an operation (+, -, *, /): " )
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation! Please enter +, -, *, or /.")
